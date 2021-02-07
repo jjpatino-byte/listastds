848a8a754409d901aa9e0a8e943fd25110754989
José <jjpatino@espol.edu.ec>
2021-02-07 10:45:23 -0500

    3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import EstudianteLayout from './Estudiante/EstudianteLayout.js';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <EstudianteLayout/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-reportWebVitals();
